49719535423630ad2deadd8d6cdd51d79bae74e8
mtpeterson <119621118+mtpeterson@users.noreply.github.com>
2025-02-04 15:01:22 -0500

    Setup basic project structure - front and back end function

---
+++ client/vite.config.js
@@ -0,0 +1,12 @@
+// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": "http://localhost:5000",
+    },
+  },
+});
